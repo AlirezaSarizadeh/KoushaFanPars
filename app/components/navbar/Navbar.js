5f6a351d4ed6b202db85cedad0acd7843323c163
Alireza Sarizadeh <a.sarizadeh@kfpdental.local>
2025-02-12 15:22:23 +0330

    installiation

---
+++ app/components/navbar/Navbar.js
@@ -0,0 +1,30 @@
+'use client'
+import { Button, Offcanvas } from "react-bootstrap";
+import React, { useState } from 'react'
+
+
+const Navbar = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow} className="me-2">
+                menu
+            </Button>
+            <Offcanvas show={show} onHide={handleClose} placement="start">
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    Some text as placeholder. In real life you can have the elements you
+                    have chosen. Like, text, images, lists, etc.
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
+    )
+}
+
+export default Navbar
---
+++ app/templates/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar />
+            Header
+        </div>
+    )
+}
+
+export default Header
