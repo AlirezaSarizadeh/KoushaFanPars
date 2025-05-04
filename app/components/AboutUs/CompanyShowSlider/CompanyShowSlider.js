9f48a55c3ed590aff55d4a0555bcf66b5fde95e4
Alireza Sarizadeh <a.sarizadeh@kfpdental.local>
2025-05-04 12:37:17 +0330

    about show companies finished

--- app/components/AboutUs/CompanyShowSlider/CompanyShowSlider.js
+++ app/components/AboutUs/CompanyShowSlider/CompanyShowSlider.js
@@ -1,16 +1,11 @@
 'use client'
 import React, { useState } from 'react'
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
-// import required modules
 import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
-
 import Title from '../../utils/title/Title';
 import Lead from '../../utils/lead/Lead';
 import Button from '../../utils/button/Button';
@@ -18,94 +13,78 @@ import Image from 'next/image';
 import images from '@/app/public/assets/images';
 import { Card } from 'react-bootstrap';
 import Link from 'next/link';
-const CompanyShowSlider = () => {
 
-      const [thumbsSwiper, setThumbsSwiper] = useState(null);
+const CompanyShowSlider = ({ flexDirection = 'row' }) => {
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+    // Determine the class for the swiper container based on flexDirection
+    const swiperContainerClass = flexDirection === 'row-reverse' ? 'col-lg-6 me-auto' : 'col-lg-6 ms-auto';
 
     return (
-        <div className='courses-box course-box-companies row p-lg-4 p-3 my-lg-3 my-2 justify-content-center col-lg-11' >
-            <div className='col-md-7 d-flex flex-column justify-content-center gap-lg-5 gap-3 align-items-between gap-2 course-box-desc'>
-                <h2 className='fs-1 fw-bold'>
-                    {/* {props.heading} */}
+        <div 
+            className='courses-box course-box-companies row p-lg-4 p-3 my-lg-3 my-2 justify-content-center col-lg-11' 
+            style={{ flexDirection: flexDirection }}
+        >
+            <div className='col-md-8 d-flex flex-column justify-content-center gap-lg-5 gap-3 align-items-between gap-2 course-box-desc'>
+                <h2 className='fs-1 fw-bold text-white'>
                     کارخانه کوشافن پارس
                 </h2>
-                <p className='' style={{ minHeight: '2' }}>
-                    امـروز سـاختار مجموعـه کوشـافن پارس بر پایـه تعامل با مشـتریان، درک نیاز و تحقق خواسـته های آنهاسـت کـه همانـا لبخنـد رضایتشـان دلگرمـی مدیریـت و پرسـنل در نیل بـه اهداف عالـی سـازمان اسـت.امیدواریم به همـت و حمایت جامعه دندانپزشـکان و دندانسـازان، دانشـگاه هـا و مراکـز علمـی بـا امیـد بـه آینـده روشـن، همواره پشـتیبان و حامـی ایـن عزیزان باشـیم.
+                <p className='text-white opacity-75 text-justify'>
+                    امـروز سـاختار مجموعـه کوشـافن پارس بر پایـه تعامل با مشـتریان، درک نیاز و تحقق خواسـته های آنهاسـت کـه همانـا لبخنـد رضایتشـان دلگرمـی مدیریـت و پرسـنل در نیل بـه اهداف عالـی سـازمان اسـت.امیدواریم به همـت و حمایت جامعه دندانپزشـکان و دندانسـازان، دانشـگاه هـا و مراکـز علمـی بـا امیـد بـه آینـده روشـن، همواره پشـتیبان و حامـی ایـن عزیزان باشـیم.امـروز سـاختار مجموعـه کوشـافن پارس بر پایـه تعامل با مشـتریان، درک نیاز و تحقق خواسـته های آنهاسـت کـه همانـا لبخنـد رضایتشـان دلگرمـی مدیریـت و پرسـنل در نیل بـه اهداف عالـی سـازمان اسـت.امیدواریم به همـت و حمایت جامعه دندانپزشـکان و دندانسـازان، دانشـگاه هـا و مراکـز علمـی بـا امیـد بـه آینـده روشـن، همواره پشـتیبان و حامـی ایـن عزیزان باشـیم.همانـا لبخنـد رضایتشـان دلگرمـی مدیریـت و پرسـنل در نیل بـه اهداف عالـی سـازمان اسـت.امیدواریم به همـت و حمایت جامعه دندانپزشـکان و دندانسـازان، دانشـگاه هـا و مراکـز علمـی بـا امیـد بـه آینـده روشـن، همواره پشـتیبان و حامـی ایـن عزیزان باشـیم.
                 </p>
             </div>
-            <div className='col-md-5 position-relative companies-pic-col'>
-                <div className='col-lg-6'>
+            <div className='col-md-4 position-relative companies-pic-col'>
+                <div className='col-lg-12'>
                     <Swiper
                         style={{
                             '--swiper-navigation-color': 'transparent',
                             '--swiper-pagination-color': '#fff',
                         }}
                         loop={true}
-                        spaceBetween={10}
                         navigation={false}
                         thumbs={{ swiper: thumbsSwiper }}
                         modules={[FreeMode, Navigation, Thumbs]}
                         className="mySwiper2"
                     >
                         <SwiperSlide>
-                            <Card className='d-flex flex-row align-items-center justify-content-between border-0'>
-                                <Image src={images.partner1_product} alt='product' style={{ objectFit: 'scale-down' }} />
-                                <Card.Body className='text-start d-flex flex-column'>
-                                    <Card.Title className='small'>کوشافن پارس 78989879889</Card.Title>
-                                    <Card.Text className='fw-light small text-justify'>
-                                        شـرکت مهندسـي تولیدي کوشـافن پارس در زمینه تولید تجهیـزات دندانپزشـکي و دندانسـازي و نیـز تامین مـواد مصرفی فعالیت می نماید شـرکت مهندسـي تولیدي کوشـافن پارس در زمینه تولید تجهیـزات دندانپزشـکي و دندانسـازي و نیـز تامین مـواد مصرفی فعالیت می نماید
-                                    </Card.Text>
-                                    <Card.Footer className='border-0 bg-transparent text-end'>
-                                        <Link className='d-flex align-items-center gap-3 textColor justify-content-end' href={'/'}>
-                                            <small className='small'>
-                                                مشاهده محصولات
-                                            </small>
-                                            <Image src={images.arrowLeftIco} alt='arrow' style={{ objectFit: 'scale-down' }} />
-                                        </Link>
-                                    </Card.Footer>
-                                </Card.Body>
+                            <Card className='w-100'>
+                                <Image src={images.room_pic} alt='product' height={300} className='w-100' style={{objectFit:'cover'}} />
                             </Card>
                         </SwiperSlide>
                         <SwiperSlide>
                             <Card className='d-flex flex-row align-items-center justify-content-between border-0'>
-                                <Image src={images.partner1_product} alt='product' style={{ objectFit: 'scale-down' }} />
-                                <Card.Body className='text-start d-flex flex-column'>
-                                    <Card.Title className='small'>کوشافن پارس</Card.Title>
-                                    <Card.Text className='fw-light small text-justify'>
-                                        شـرکت مهندسـي تولیدي کوشـافن پارس در زمینه تولید تجهیـزات دندانپزشـکي و دندانسـازي و نیـز تامین مـواد مصرفی فعالیت می نماید شـرکت مهندسـي تولیدي کوشـافن پارس در زمینه تولید تجهیـزات دندانپزشـکي و دندانسـازي و نیـز تامین مـواد مصرفی فعالیت می نماید
-                                    </Card.Text>
-                                    <Card.Footer className='border-0 bg-transparent text-end'>
-                                        <Link className='d-flex align-items-center gap-3 textColor justify-content-end' href={'/'}>
-                                            <small className='small'>
-                                                مشاهده محصولات
-                                            </small>
-                                            <Image src={images.arrowLeftIco} alt='arrow' style={{ objectFit: 'scale-down' }} />
-                                        </Link>
-                                    </Card.Footer>
-                                </Card.Body>
+                                <Image src={images.room_pic} alt='product' height={300} className='w-100' style={{objectFit:'cover'}} />
                             </Card>
                         </SwiperSlide>
                         <SwiperSlide>
                             <Card className='d-flex flex-row align-items-center justify-content-between border-0'>
-                                <Image src={images.partner1_product} alt='product' style={{ objectFit: 'scale-down' }} />
-                                <Card.Body className='text-start d-flex flex-column'>
-                                    <Card.Title className='small'>کوشافن پارس 45454</Card.Title>
-                                    <Card.Text className='fw-light small text-justify'>
-                                        شـرکت مهندسـي تولیدي کوشـافن پارس در زمینه تولید تجهیـزات دندانپزشـکي و دندانسـازي و نیـز تامین مـواد مصرفی فعالیت می نماید شـرکت مهندسـي تولیدي کوشـافن پارس در زمینه تولید تجهیـزات دندانپزشـکي و دندانسـازي و نیـز تامین مـواد مصرفی فعالیت می نماید
-                                    </Card.Text>
-                                    <Card.Footer className='border-0 bg-transparent text-end'>
-                                        <Link className='d-flex align-items-center gap-3 textColor justify-content-end' href={'/'}>
-                                            <small className='small'>
-                                                مشاهده محصولات
-                                            </small>
-                                            <Image src={images.arrowLeftIco} alt='arrow' style={{ objectFit: 'scale-down' }} />
-                                        </Link>
-                                    </Card.Footer>
-                                </Card.Body>
+                                <Image src={images.room_pic} alt='product' height={300} className='w-100' style={{objectFit:'cover'}} />
                             </Card>
                         </SwiperSlide>
                     </Swiper>
                 </div>
+            </div>
+            <div className={swiperContainerClass}>
+                <Swiper
+                    onSwiper={setThumbsSwiper}
+                    loop={true}
+                    spaceBetween={20}
+                    slidesPerView={3}
+                    freeMode={true}
+                    watchSlidesProgress={true}
+                    modules={[FreeMode, Navigation, Thumbs]}
+                    className="mySwiper mt-lg-4 mt-2 partner-logo-swiper"
+                >
+                    <SwiperSlide className='rounded-2' style={{height:'fit-content' , overflow:'hidden'}}>
+                        <Image src={images.room_pic} width={150} height={120} className='w-100' alt='partner' />
+                    </SwiperSlide>
+                    <SwiperSlide className='rounded-2' style={{height:'fit-content' , overflow:'hidden'}}>
+                        <Image src={images.room_pic} width={150} height={120} className='w-100' alt='partner' />
+                    </SwiperSlide>
+                    <SwiperSlide className='rounded-2' style={{height:'fit-content' , overflow:'hidden'}}>
+                        <Image src={images.room_pic} width={150} height={120} className='w-100' alt='partner' />
+                    </SwiperSlide>
+                </Swiper>
             </div>
         </div>
     )
--- app/public/assets/images.js
+++ app/public/assets/images.js
@@ -60,6 +60,7 @@ import cut_loc from './images/location-ctu.png'
 import ctu_cart from './images/sms-ctu.png'
 import ctu_mail from './images/call-ctu.png'
 import company_1 from './images/companies-1.png'
+import room_pic from './images/room-pic.png'
 
 const images = {
    mainLogo,
@@ -123,6 +124,7 @@ const images = {
    cut_loc,
    ctu_cart,
    ctu_mail,
-   company_1
+   company_1,
+   room_pic
 }
 export default images
